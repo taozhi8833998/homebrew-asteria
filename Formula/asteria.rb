ceac62391ddebb4950d3314395e7edaf52c2ad6d
taozhi8833998 <taozhi8833998@163.com>
2022-05-17 14:42:20 +0800

    fix: deploy api ip type

--- Formula/asteria.rb
+++ Formula/asteria.rb
@@ -1,9 +1,9 @@
 class Asteria < Formula
   desc "CLI for asteria written in Rust"
   homepage "https://github.com/taozhi8833998/asteria-cli"
-  url "https://github.com/taozhi8833998/asteria-cli/releases/download/1.9.2/asteria-1.9.2-x86_64-apple-darwin.tar.gz"
-  sha256 "5282b2815633e7c992546ef58996d27d811a9fc3c0eb33ca4bbabdf36fcf9cef"
-  version "1.9.2"
+  url "https://github.com/taozhi8833998/asteria-cli/releases/download/1.9.3/asteria-1.9.3-x86_64-apple-darwin.tar.gz"
+  sha256 "4b6cfc428a9e0296768e2697e33326615bf70a87d7fe7cbeefa2a33df961b425"
+  version "1.9.3"
 
   def install
     bin.install "asteria"
