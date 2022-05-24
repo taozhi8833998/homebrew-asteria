ff3dce1351bbd72521fb2bce097c876a2b95b43c
taozhi8833998 <taozhi8833998@163.com>
2022-05-24 14:43:11 +0800

    refactor: upgrade version

--- Formula/asteria.rb
+++ Formula/asteria.rb
@@ -1,9 +1,9 @@
 class Asteria < Formula
   desc "CLI for asteria written in Rust"
   homepage "https://github.com/taozhi8833998/asteria-cli"
-  url "https://github.com/taozhi8833998/asteria-cli/releases/download/1.9.3/asteria-1.9.3-x86_64-apple-darwin.tar.gz"
-  sha256 "4b6cfc428a9e0296768e2697e33326615bf70a87d7fe7cbeefa2a33df961b425"
-  version "1.9.3"
+  url "https://github.com/taozhi8833998/asteria-cli/releases/download/1.9.4/asteria-1.9.4-x86_64-apple-darwin.tar.gz"
+  sha256 "29448388117094b1227a94f7478e7d0cad4f0182f6009d5377d6ebe5eb919850"
+  version "1.9.4"
 
   def install
     bin.install "asteria"
